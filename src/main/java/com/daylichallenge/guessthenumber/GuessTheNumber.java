8bb9e44194274f4fc545bf8267414780a8163d46
MamadouKernel <kernel0748@gmail.com>
2023-02-03 16:59:29 +0000

    update du dailyChallenge

--- src/main/java/com/daylichallenge/guessthenumber/GuessTheNumber.java
+++ src/main/java/com/daylichallenge/guessthenumber/GuessTheNumber.java
@@ -4,8 +4,8 @@
 
 package com.daylichallenge.guessthenumber;
 
-import java.util.Scanner;
 import java.util.Random;
+import java.util.Scanner;
 
 /**
  *
@@ -13,29 +13,32 @@ import java.util.Random;
  */
 public class GuessTheNumber {
 
-	public static void main(String[] args) {
+  public static void main(String[] args) {
+    try (Scanner sc = new Scanner(System.in)) {
+      Random rand = new Random();
 
-		try (Scanner sc = new Scanner(System.in)) {
-			Random rand = new Random();
+      int targetNumber = rand.nextInt(100) + 1;
+      int guess;
+      int attempts = 0;
 
-			int targetNumber = rand.nextInt(100) + 1;
-			int guess;
-			int attempts = 0;
-			
-			System.out.print("Devine le nombre (1 à 100): ");
-                        
-			do {
-				guess = sc.nextInt();
-				attempts++;
-				if (guess > targetNumber) {
-					 System.out.println("Le nombre est plus petit!");
-				} else if (guess < targetNumber) {
-					System.out.println("Le nombre est plus grand!");
-				}
-			} while (guess != targetNumber);
+      System.out.print(targetNumber);
 
-			System.out.println("Félicitations! Tu as deviné le nombre en " + attempts + " tentatives.");
-			
-		}
-	}
+      System.out.print("Devine le nombre (1 à 100): ");
+
+      do {
+        guess = sc.nextInt();
+        attempts++;
+        if (guess > targetNumber) {
+          System.out.println("Le nombre est plus petit!");
+        } else if (guess < targetNumber) {
+          System.out.println("Le nombre est plus grand!");
+        }
+      } while (guess != targetNumber && attempts < 5);
+	  if(guess == targetNumber) {
+      	System.out.println("Félicitations! Tu as deviné le nombre en " + attempts + " tentatives.");
+	  } else{
+		System.out.println("Tu as perdu! Le nombre était " + targetNumber);
+	  }
+    }
+  }
 }
